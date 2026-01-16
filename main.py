6f73a06f9381557b4c88fdcdc368fae8335b3263
daniget12 <danielgetahun1000@gmail.com>
2026-01-16 22:03:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ CATEGORIES = [
     "Harassment", "Sexual",
     
     # Other Topics
-    "Religion", "Mental", "Funny Story", "joke", "meme",
+    "Religion", "Mental", "Funny Story", "Joke", "Meme",
     
     # Help & Support
     "Question", "Advice Needed",
@@ -1730,6 +1730,7 @@ if __name__ == "__main__":
         asyncio.run(main())
     except KeyboardInterrupt:
         logging.info("Bot stopped by user.")
+
 
 
 
