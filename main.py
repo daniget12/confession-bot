fa38cfe3c5d87c01ccd5dec2572881154dd60c00
Dani G <danielgetahun1000@gmail.com>
2026-05-21 21:04:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1428,8 +1428,8 @@ async def show_top_aura(message: types.Message):
             # Generate profile link
             profile_link = await get_encoded_profile_link(user['user_id'])
             
-            response_text += f"{prefix}<b>{html.quote(user['profile_name'])}</b> "
-            response_text += f"🏅 {user['points']} points"
+            response_text += f"{prefix}<b>{html.quote(user['profile_name'])}</b> \n "
+            response_text += f"🏅 {user['points']} points \n "
             
 
             
